56caa39bb6f3244d862f0d03f0e327c75a6869b1
Ivan Shcheklein <shcheklein@gmail.com>
2025-06-14 12:59:11 -0700

    use fork strategy to run on mac/linux

--- computer_vision/fashion_product_images/scripts/3-train-model.py
+++ computer_vision/fashion_product_images/scripts/3-train-model.py
@@ -1,8 +1,10 @@
+import multiprocessing
 from torch.utils.data import DataLoader
 
 import datachain as dc
 from datachain.lib.pytorch import label_to_int
 from src.train import train_model, transform
+
 
 # Define classes
 
@@ -24,23 +26,26 @@ NUM_CLASSES = len(CLASSES)
 def add_target_label(usage) -> str:
     return usage if usage in CLASSES else "nan"
 
-ds = (
-    dc.read_dataset("fashion-train")
-    .map(target=add_target_label, params=["usage"], output=str)
-    .map(label=lambda target: label_to_int(target, CLASSES), output=int)
-    .limit(1000) # Take a sample for the DEMO purposes
-    .shuffle()
-)
+if __name__ == '__main__':
+    ds = (
+        dc.read_dataset("fashion-train")
+        .map(target=add_target_label, params=["usage"], output=str)
+        .map(label=lambda target: label_to_int(target, CLASSES), output=int)
+        .limit(1000) # Take a sample for the DEMO purposes
+        .shuffle()
+    )
 
-print(ds.to_pandas().target.value_counts())
+    print(ds.to_pandas().target.value_counts())
 
-# PyTorch DataLoader
-
-train_loader = DataLoader(
-    ds.select("file", "label").to_pytorch(transform=transform),
-    batch_size=2
-)
-
-# Train the model
-
-model, optimizer = train_model(train_loader, NUM_CLASSES, num_epochs=3, lr=0.001)
+    # PyTorch DataLoader with multiprocessing
+    train_loader = DataLoader(
+        ds.select("file", "label").to_pytorch(transform=transform),
+        batch_size=2,
+        num_workers=4,  # Use multiple workers for better performance
+        # Might need to adjust this depending on your system
+        # and if you want to use multiple workers
+        multiprocessing_context=multiprocessing.get_context("fork"),
+    )
+    
+    # Train the model
+    model, optimizer = train_model(train_loader, NUM_CLASSES, num_epochs=1, lr=0.001)
